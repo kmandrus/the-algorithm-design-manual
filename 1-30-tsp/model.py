5131c6df1c399df0fbda68122de6b9f44ea4495e
Kevin Andrus <kevin.andrus@gmail.com>
2022-12-11 11:22:39 -0500

    apply black formatter to a few files

--- 1-30-tsp/model.py
+++ 1-30-tsp/model.py
@@ -4,12 +4,12 @@ from typing import List, Tuple
 
 
 # Helpers
-def get_random_points(plane_size: int, number_of_points:int) -> set:
-    """ 
+def get_random_points(plane_size: int, number_of_points: int) -> set:
+    """
     A collection of randomly generated points for the Traveling Salesman Problem.
-    
+
     Inputs: size (edge length) the containing square plane, number of points to generate.
-    Output: set of points 
+    Output: set of points
     Points are unique tuples of integers.
     """
     points = set()
@@ -18,24 +18,25 @@ def get_random_points(plane_size: int, number_of_points:int) -> set:
     return points
 
 
-def compute_tour_length(tour: List[Tuple[int,int]]) -> float:
+def compute_tour_length(tour: List[Tuple[int, int]]) -> float:
     tour_length = 0
-    # start with previous_point set the final point b/c this is a closed tour 
+    # start with previous_point set the final point b/c this is a closed tour
     previous_point = tour[-1]
     for point in tour:
         tour_length += dist(point, previous_point)
-        previous_point = point 
+        previous_point = point
     return tour_length
-        
+
 
 class Link:
     """
     A node for a linked list.
     """
-    def __init__(self, value, next = None):
+
+    def __init__(self, value, next=None):
         self.value = value
         self.next = next
-    
+
     @property
     def head(self):
         current_link = self
@@ -49,7 +50,7 @@ class Link:
         while current_link.next:
             current_link = current_link.next
         return current_link
-    
+
     def to_list(self):
         current_node = self.head
         node_list = [current_node]
@@ -57,25 +58,27 @@ class Link:
             node_list.append(current_node.value)
         return node_list
 
-    def is_member(self, other) -> bool: 
+    def is_member(self, other) -> bool:
         current = self.head
-        while current.next: 
+        while current.next:
             if current.value == other.value:
                 return True
         return False
 
+
 class LinkedList:
     def __init__(self):
         pass
-    
-# I think we should delete this, in favor of a list or dictionary. If the graph doesn't 
+
+
+# I think we should delete this, in favor of a list or dictionary. If the graph doesn't
 # use this Edge class, it will confuse me down the line.
 class Edge:
     def __init__(self, start: Link, end: Link):
-        self.start = start 
+        self.start = start
         self.end = end
         self.length = dist(self.start.value, self.end.value)
-    
+
     def connect(self):
         if not self.start.previous and not self.end.next:
             self.start.previous = self.end
@@ -90,14 +93,14 @@ class Edge:
 
 class Graph:
     """
-    Represents a collection of verticies and edges. 
+    Represents a collection of verticies and edges.
 
     A 'vertex' is a tuple of integers.
     An 'edge' is a set of two vertices.
     A 'chain' is a set of connected vertices without branches.
     """
 
-    def __init__(self, verticies, edges = None):
+    def __init__(self, verticies, edges=None):
         self._verticies = verticies
         self._edges = edges
 
@@ -121,7 +124,7 @@ class Graph:
 
     def connect(self, vertex_1, vertex_2):
         pass
-    
+
     def disconnect(self, vertex_1, vertex_2):
         pass
 
